72c382177657cc4d544107a08a2f0eb1a06a8f52
amyciav <amyciav@4d29bf45-b0ad-fbb4-8f23-08e071a99dba>
2010-11-13 01:31:24 +0000

    Created xCode Project

---
+++ FinalProject/Classes/FinalProjectAppDelegate.h
@@ -0,0 +1,33 @@
+//
+//  FinalProjectAppDelegate.h
+//  FinalProject
+//
+//  Created by Amy Ciavolino on 11/12/10.
+//  Copyright Single Line Softwear 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface FinalProjectAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    UINavigationController *navigationController;
+
+@private
+    NSManagedObjectContext *managedObjectContext_;
+    NSManagedObjectModel *managedObjectModel_;
+    NSPersistentStoreCoordinator *persistentStoreCoordinator_;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
+
---
+++ FinalProject/Classes/RootViewController.h
@@ -0,0 +1,22 @@
+//
+//  RootViewController.h
+//  FinalProject
+//
+//  Created by Amy Ciavolino on 11/12/10.
+//  Copyright Single Line Softwear 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface RootViewController : UITableViewController <NSFetchedResultsControllerDelegate> {
+
+@private
+    NSFetchedResultsController *fetchedResultsController_;
+    NSManagedObjectContext *managedObjectContext_;
+}
+
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+
+@end
