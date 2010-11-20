5762001f0fd00a0c54fdae2a7c09cb50cd2b8a0a
amyciav <amyciav@4d29bf45-b0ad-fbb4-8f23-08e071a99dba>
2010-11-20 17:14:37 +0000

    Added p-list file and code to pull it into a dictionary take 3

---
+++ trunk/FinalProject/Classes/ConferenceObjectViewController.h
@@ -0,0 +1,16 @@
+//
+//  ConferenceObjectViewController.h
+//  FinalProject
+//
+//  Created by Amy Ciavolino on 11/19/10.
+//  Copyright 2010 UMBC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ConferenceObjectViewController : UIViewController {
+
+}
+
+@end
