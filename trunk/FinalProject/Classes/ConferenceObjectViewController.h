c0ff905df15fbc067a5e445fd685d08bcf014531
timothy.e.burke@gmail.com <timothy.e.burke@gmail.com@4d29bf45-b0ad-fbb4-8f23-08e071a99dba>
2010-11-20 21:51:24 +0000

    Corrected ConferenceObjectViewController to properly display the conference objects.

--- trunk/FinalProject/Classes/ConferenceObjectViewController.h
+++ trunk/FinalProject/Classes/ConferenceObjectViewController.h
@@ -16,7 +16,6 @@
 }
 
 @property (nonatomic, retain) ConferenceObject *confObj;
-@property (nonatomic, retain) IBOutlet UIImageView *image;
 @property (nonatomic, retain) IBOutlet UITextView *textView;
 @property (nonatomic, retain) IBOutlet UIScrollView *scrollView;
 @property (nonatomic, retain) IBOutlet UIBarButtonItem *favoriteButton;
