d54aea5bb1d846a9c14b688d177220a8d2c8d6d3
timothy.e.burke@gmail.com <timothy.e.burke@gmail.com@4d29bf45-b0ad-fbb4-8f23-08e071a99dba>
2010-11-25 00:19:39 +0000

    Updated map view with proper zooming and centering for double tap and multitouch pinch-zoom.

--- FinalProject/Classes/MapViewController.m
+++ FinalProject/Classes/MapViewController.m
@@ -12,6 +12,8 @@
 @implementation MapViewController
 
 @synthesize scrollView;
+@synthesize imageView;
+@synthesize gestureRecognizer;
 
 @synthesize confObj;
 
@@ -19,62 +21,98 @@
 - (void)viewWillAppear:(BOOL)animated {
 	[super viewWillAppear:animated];
 	
+	self.gestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleGesture:)];
+	
+	[(UITapGestureRecognizer *)self.gestureRecognizer setNumberOfTapsRequired:2];
+	
+	self.gestureRecognizer.delegate = self;
+	
+	[self.view addGestureRecognizer:self.gestureRecognizer];
+	
+	// Set the title
+	self.title = @"Map";
+	
+	// Set scrollview delegate
 	self.scrollView.delegate = self;
 	
-	UIImageView *iv = [[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"map"]] autorelease];
+	// Initialize the map image
+	self.imageView = [[[UIImageView alloc] initWithImage:[UIImage imageNamed:@"map"]] autorelease];
 	
-	[self.scrollView addSubview:iv];
-	
-	self.scrollView.contentSize = CGSizeMake(iv.image.size.height, iv.image.size.width);
-	
-	[self.scrollView setContentOffset:CGPointMake((iv.image.size.height - 320) / 2, (iv.image.size.width - 460) / 2)];
-	
-	
-	/*
-	UIBarButtonItem *temp = [[[UIBarButtonItem alloc] initWithTitle:@"Zoom" 
-															  style:UIBarButtonItemStyleBordered 
-															 target:self 
-															 action:@selector(myZoom)] autorelease];
-	
-	self.navigationItem.rightBarButtonItem = temp;
-	
-	self.scrollView.minimumZoomScale = 0.0;
-	self.scrollView.maximumZoomScale = 3.0;
-	*/
-	
+	// Draw the dot on the image if there is a location associated
 	if(self.confObj) {
 		// Code to draw dot will go here
 		
 	}
 	
+	// Add the image to the scrollview
+	[self.scrollView addSubview:self.imageView];
 	
+	// Set the content size of the scrollview to the size of the image
+	self.scrollView.contentSize = CGSizeMake(self.imageView.image.size.height, self.imageView.image.size.width);
+	
+	// scroll to the center of the image
+	[self setCenter:CGPointMake(self.imageView.image.size.width / 2, self.imageView.image.size.height / 2) animated:NO];
+	 
+		// Set the zoom scale for the scrollview
+	self.scrollView.minimumZoomScale = 0.5;
+	self.scrollView.maximumZoomScale = 3.0;
+
 	
 }
 
-
-/*
-- (void)myZoom {
+- (void)setCenter:(CGPoint)point animated:(BOOL)animated {
+	//[self.scrollView setCenter:point];
 	
-	if(self.scrollView.zoomScale == 2.0) {
-		[self.scrollView setZoomScale:1.0 animated:YES];
-		self.scrollView.contentSize = CGSizeMake(1024, 1024);
-	} else {
-		[self.scrollView setZoomScale:2.0 animated:YES];
-		self.scrollView.contentSize = CGSizeMake(1024 * 1.15, 1024 * 1.2);
+	// Translate the point into a content offset
+	int x = point.x - (self.scrollView.frame.size.width / 2);
+	int y = point.y - (self.scrollView.frame.size.height / 2);
+	
+	// Check bounds of the map
+	if(x < 0) {
+		x = 0;
 	}
+	if(x > self.scrollView.contentSize.width - self.scrollView.frame.size.width) {
+		x = self.scrollView.contentSize.width - self.scrollView.frame.size.width;
+	}
+	if(y < 0) {
+		y = 0;
+	}
+	if(y > self.scrollView.contentSize.height - self.scrollView.frame.size.height) {
+		y = self.scrollView.contentSize.height - self.scrollView.frame.size.height;
+	}
+	
+	// If at max zoom scale, override and animate, even if told not to
+	if(self.scrollView.zoomScale == self.scrollView.maximumZoomScale) {
+		animated = YES;
+	}
+	
+	// Center the map
+	[self.scrollView setContentOffset:CGPointMake(x, y) animated:animated];
 }
- */
 
 
 
-/*
-- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollViewt {
-	return scrollViewt;
+- (void)handleGesture:(UIGestureRecognizer *)gestureRecognizerr {
+	//NSLog(@"Double Tap!");
+	
+	CGPoint temp = [gestureRecognizerr locationInView:self.scrollView];
+	
+	NSLog(@"%f - %f", temp.x, temp.y);
+	
+	[self setCenter:temp animated:NO];
+	
+	float scale = self.scrollView.zoomScale * 2.0;
+	[self.scrollView setZoomScale:scale animated:YES];
+}
+
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+	return self.imageView;
 }
 
 - (void)scrollViewDidEndZooming:(UIScrollView *)scrollView withView:(UIView *)view atScale:(float)scale {
-	
-}*/
+	//NSLog(@"scale:%f", scale);
+}
 
 
 
@@ -100,6 +138,8 @@
 
 - (void)dealloc {
 	self.confObj = nil;
+	self.scrollView = nil;
+	self.imageView = nil;
     [super dealloc];
 }
 
