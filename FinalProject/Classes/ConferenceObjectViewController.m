9b63dc77def2a72d0c4cb80ed4e201fcf880b8d7
timothy.e.burke@gmail.com <timothy.e.burke@gmail.com@4d29bf45-b0ad-fbb4-8f23-08e071a99dba>
2010-12-03 03:03:44 +0000

    Made more awesome the menus by adding images to the tableview cells.

--- FinalProject/Classes/ConferenceObjectViewController.m
+++ FinalProject/Classes/ConferenceObjectViewController.m
@@ -30,22 +30,24 @@
 #pragma mark IBActions
 
 - (void)favoriteButtonPressed {
+	// Toggle the state of the objects favorite status and swap the image
 	self.confObj.favorite = !self.confObj.favorite;
 	if(self.confObj.favorite) {
 		self.favoriteButton.image = [UIImage imageNamed:@"FavIcon_T"];
 	} else {
 		self.favoriteButton.image = [UIImage imageNamed:@"FavIcon_F"];
 	}
+	
+	// Set the setting of that object as a favorite to persist across application runs
 	[self.defaults setBool:self.confObj.favorite forKey:[NSString stringWithFormat:@"%d", self.confObj.IDnum]];
 	[self.defaults synchronize]; 
 }
 
 - (IBAction)locationButtonPressed {
+	// Init the map view controller, pass along the conference object and then
+	// display the map (with location pin)
 	MapViewController *mv = [[[MapViewController alloc] init] autorelease];
 	mv.confObj = self.confObj;
-	
-	
-	
 	[self.navigationController pushViewController:mv animated:YES];
 }
 
@@ -55,6 +57,7 @@
 - (void)viewWillAppear:(BOOL)animated {
 	[super viewWillAppear:animated];
 	
+	// Set the title of the view to the type of Conference Object (saves space over name)
 	switch (self.confObj.type) {
 		case EventType:
 			self.title = @"Event";
@@ -70,22 +73,30 @@
 			break;
 	}
 	
+	// Set the text fields (label and textview)
 	self.objTitle.text = self.confObj.title;
 	self.textView.text = self.confObj.description;
 	
+	// Start a running total of height
 	int scrollViewHeight = 40;
 	
+	// Calculate the scaled height of the image
 	int scaledImageHeight = ((280 * self.confObj.image.size.height) / self.confObj.image.size.width);
-	
-	//CGRectMake(<#CGFloat x#>, <#CGFloat y#>, <#CGFloat width#>, <#CGFloat height#>)
-	
-	UIImageView *image = [[UIImageView alloc] initWithFrame:CGRectMake(20, 40, 280, scaledImageHeight)];
+
+	// Init the image with the necessary size and add to the scrollview
+	UIImageView *image = [[UIImageView alloc] initWithFrame:CGRectMake(20, scrollViewHeight, 280, scaledImageHeight)];
 	image.image = self.confObj.image;
 	[self.scrollView addSubview:image];
+	
+	// Add the height of the image to the running total
 	scrollViewHeight += scaledImageHeight;
 	
+	// If the object type is event, display a subview containing the times of event
+	// and a location button to bring up the map of hte location
 	if(self.confObj.type == EventType) {
 		scrollViewHeight += 5;
+		
+		// Put the subview in the correct position in the subview
 		self.timeLocationView.center = CGPointMake(self.timeLocationView.center.x, scrollViewHeight + (self.timeLocationView.frame.size.height / 2));
 		scrollViewHeight += self.timeLocationView.frame.size.height;
 		self.timeLocationView.hidden = NO;
@@ -105,6 +116,7 @@
 			self.lblTime.text = [dateFormatter stringFromDate:confObj.startTime];
 		}
 		
+		// Set the map button to the name of the location
 		[self.locationButton setTitle:@"Location Name" forState:UIControlStateNormal];
 		[self.locationButton setTitle:@"Location Name" forState:UIControlStateHighlighted];
 		
@@ -113,17 +125,22 @@
 	}
 	scrollViewHeight += 5;
 	
+	// Adjust the UITextView to the height of its content so it does not
+	// scroll itself, but expands to the size of the text within
 	CGRect tempFrame = self.textView.frame;
 	tempFrame.size.height = self.textView.contentSize.height;
 	self.textView.frame = tempFrame;
 	
-	
+	// Position the textview accordingly
 	self.textView.center = CGPointMake(self.textView.center.x, scrollViewHeight + (self.textView.frame.size.height / 2));
 	
+	// Add some padding to the bottom
 	scrollViewHeight += self.textView.frame.size.height + 20;
 	
+	// Set the scrolling size of the scrollView
 	self.scrollView.contentSize = CGSizeMake(320, scrollViewHeight);
 	
+	// Init and add the favorite button to the navigation controller
 	self.favoriteButton = [[[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"FavIcon_F"] 
 															style:UIBarButtonItemStylePlain 
 														   target:self 
@@ -147,8 +164,11 @@
 #pragma mark -
 #pragma mark Memory Management
 
+
 - (void)viewWillDisappear:(BOOL)animated {
 	[super viewWillDisappear:animated];
+	
+	// Remove the favorite button from the navigation controller
 	self.navigationController.navigationItem.rightBarButtonItem = nil;
 }
 
