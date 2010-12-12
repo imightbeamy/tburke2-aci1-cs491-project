6849de04a94a31b3807988c3ee9c3faf086fc653
timothy.e.burke@gmail.com <timothy.e.burke@gmail.com@4d29bf45-b0ad-fbb4-8f23-08e071a99dba>
2010-12-12 21:50:41 +0000

    Comments in ConferenceObjectViewController.h

--- trunk/FinalProject/Classes/ConferenceObjectViewController.h
+++ trunk/FinalProject/Classes/ConferenceObjectViewController.h
@@ -18,44 +18,66 @@
 
 }
 
-// The conference object being displayed
+/**
+ The conference object being displayed
+*/
 @property (nonatomic, retain) ConferenceObject *confObj;
 
-// A textview to display the description of the conference object
+/*
+ A textview to display the description of the conference object
+*/
 @property (nonatomic, retain) IBOutlet UITextView *textView;
 
-// A scrollview property that contains all of the displayed conference
-// object elements, made scrollable to allow more information than can
-// be fit on screen to be displayed and scrolled through by the user.
+/*
+ A scrollview property that contains all of the displayed conference
+ object elements, made scrollable to allow more information than can
+ be fit on screen to be displayed and scrolled through by the user.
+*/
 @property (nonatomic, retain) IBOutlet UIScrollView *scrollView;
 
-// The favorite button inserted into the navigation bar
+/*
+ The favorite button inserted into the navigation bar
+*/
 @property (nonatomic, retain) IBOutlet UIBarButtonItem *favoriteButton;
 
-// A label to display the time in a conference object
+/*
+ A label to display the time in a conference object
+*/
 @property (nonatomic, retain) IBOutlet UILabel *lblTime;
 
-// A UIView inserted into the scroll view to display the the location
-// button and time of an event conference object
+/*
+ A UIView inserted into the scroll view to display the the location
+ button and time of an event conference object
+*/
 @property (nonatomic, retain) IBOutlet UIView *timeLocationView;
 
-// A button to display the title of the location for an event
-// conference object, and open a map view if pressed
+/*
+ A button to display the title of the location for an event
+ conference object, and open a map view if pressed
+*/
 @property (nonatomic, retain) IBOutlet UIButton *locationButton;
 
-// A label to display the title of the object
+/*
+ A label to display the title of the object
+*/
 @property (nonatomic, retain) IBOutlet UILabel *objTitle;
 
-// A defaults object to allow persistence of favorite settings
+/*
+ A defaults object to allow persistence of favorite settings
+*/
 @property (nonatomic, retain) NSUserDefaults * defaults;
 
-// A function to toggle the status of the favorite button and the
-// setting for favorite on a specific conference object
+/*
+ A function to toggle the status of the favorite button and the
+ setting for favorite on a specific conference object
+*/
 - (void)favoriteButtonPressed;
 
-// A function launched when the location button is pressed to push
-// a map view controller onto the view stack and display a map
-// with a marker indicating the event location
+/*
+ A function launched when the location button is pressed to push
+ a map view controller onto the view stack and display a map
+ with a marker indicating the event location
+*/
 - (IBAction)locationButtonPressed;
 
 @end
