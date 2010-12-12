130737f78c3744508e79c3f7b8f800cd3a7fd70d
amyciav <amyciav@4d29bf45-b0ad-fbb4-8f23-08e071a99dba>
2010-12-12 07:43:59 +0000

    Commented ConfObj.[hm]. Corrected Spelling of location property and added location to dealloc method.

--- trunk/FinalProject/ConferenceObject.h
+++ trunk/FinalProject/ConferenceObject.h
@@ -8,38 +8,111 @@
 
 #import <CoreData/CoreData.h>
 
+/**
+ An enumeration representing the types of items included in a conference
+ program. 
+ */
 typedef enum {
-	kEventType,
-	kSponsorType,
-	kSpeakerType,
-	kUnknownConfType
+	kEventType,			/*Represents conference events*/
+	kSponsorType,		/*Represents Sponsors of the conference*/
+	kSpeakerType,		/*Represents Speakers at the conference*/
+	kUnknownConfType	/*Represents conference items that are invalid/unkown*/
 } ConferenceObjectType;
 
+
+/**
+ A class that models an item of information in a conference program.
+ */
 @interface ConferenceObject:  NSObject  
 {
 	
 }
 
-@property (nonatomic, retain) NSString * title;
-@property (nonatomic, retain) UIImage * image;
-@property (nonatomic, retain) NSString * description;
-@property bool favorite;
-@property (nonatomic, retain) NSDate * startTime;
-@property (nonatomic, retain) NSDate * endTime;
+/**
+ The type of this conference info item. (e.g. Event, Speaker, Sponsor)
+ */
 @property ConferenceObjectType type;
+
+/**
+ The title of an event or name of speaker/sponsor.
+ */
+@property (nonatomic, retain) NSString * title;
+
+/**
+ The image to be displayed for the this conference info item. Shown in both the
+ menu table views and the detailed view of the item.
+ */
+@property (nonatomic, retain) UIImage * image;
+
+/**
+ A description of the event/sponsor or bio for a speaker.
+ */
+@property (nonatomic, retain) NSString * description;
+
+/**
+ Whether this item has been added to the user's favorites.
+ */
+@property bool favorite;
+
+/**
+ A unique identification number for this conference info item. 
+ Used for keeping track of persisted favorites 
+ */
 @property int IDnum;
 
-@property (nonatomic, retain) NSString * loction;
+/**
+ The starting time of an event. Nil for speaker/sponsor.
+ */
+@property (nonatomic, retain) NSDate * startTime;
+
+/**
+ The ending time of an event. Nil for speaker/sponsor.
+ */
+@property (nonatomic, retain) NSDate * endTime;
+
+/**
+  The name of the location of an event. Nil for speaker/sponsor.
+ */
+@property (nonatomic, retain) NSString * location;
+
+/**
+  The location of of an event on the map in pixels. 0,0 for speaker/sponsor.
+ */
 @property int x;
 @property int y;
 
-
+/**
+ Returns a string that represents the ConferenceObjectType of this item.
+ */
 @property (readonly) NSString * typeString;
+
+/**
+ Returns a string that has all the searchable text from the info.
+ */
 @property (readonly) NSString * searchableText;
 
+/**
+ Returns an autoreleased ConferenceObject with the information filled
+ out with values from the given dictionary.
+ */
 + (id)ConferenceObjectFromDictionary: (NSDictionary *) dictionary;
+
+/**
+ Returns a ConferenceObjectType that matches the string passed in. If the
+ String matches no type, kUnknownConfType is returned. Used when reading 
+ info in from the plist.
+*/
 + (ConferenceObjectType) typeFromString: (NSString *) dicType;
+
+/**
+ Convenience method that returns an NSComparator for sorting Events by time.
+ */
 + (NSComparator) getEventTimeComparator;
+
+/**
+ Convenience method that returns an NSComparator for sorting any conference 
+ info item alphibeticly by title.
+ */
 + (NSComparator) getTitleComparator;
 
 @end
