0cfbb6903999dabe2485ee4e070f6c616acece1b
amyciav <amyciav@4d29bf45-b0ad-fbb4-8f23-08e071a99dba>
2010-12-13 02:24:05 +0000

    Added comments to AboutView [mh]

--- FinalProject/Classes/AboutViewController.m
+++ FinalProject/Classes/AboutViewController.m
@@ -2,8 +2,8 @@
 //  AboutViewController.m
 //  FinalProject
 //
-//  Created by Amy Ciavolino on 12/12/10.
-//  Copyright 2010 UMBC. All rights reserved.
+//  Amy Ciavolino and Tim Burke
+//  CMSC491 - Fall 2010
 //
 
 #import "AboutViewController.h"
@@ -13,18 +13,6 @@
 
 @synthesize aboutText;
 
-/*
- // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
-        // Custom initialization
-    }
-    return self;
-}
-*/
-
-
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
     [super viewDidLoad];
 	self.title = @"About";
@@ -35,28 +23,6 @@
 													error:NULL];
 	self.aboutText.text = blurb;
 }
-
-/*
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
-}
-*/
-
-- (void)didReceiveMemoryWarning {
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
-}
-
-- (void)viewDidUnload {
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
-    // e.g. self.myOutlet = nil;
-}
-
 
 - (void)dealloc {
     [super dealloc];
